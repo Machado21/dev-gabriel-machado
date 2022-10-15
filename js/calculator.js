36013e77c8238f94514c5cc4c979addbcdda70eb
Gabriel Machado <grmachado12@hotmail.com>
2022-10-15 07:25:15 -0400

    feat: functions of the calculator

--- js/calculator.js
+++ js/calculator.js
@@ -1,13 +1,99 @@
-function sum(params) {
-  var number_one = document.querySelector(".number_one");
-  var number_two = document.querySelector(".number_two");
-  var result = document.querySelector(".lab_result");
+function operator(oper) {
+
+  const operator = document.querySelector(".lab_operator");
+
+  console.log(oper);
+
+  switch (oper) {
+    case "+":
+      operator.innerHTML = "+";
+      break;
+    case "-":
+      operator.innerHTML = "-";
+      break;
+    case "*":
+      operator.innerHTML = "*";
+      break;
+    case "/":
+      operator.innerHTML = "/";
+
+      break;
+
+    default:
+      operator.innerHTML = ":x";
+
+      break;
+  }
+}
+
+function result() {
+  const operator = document.querySelector(".lab_operator");
+  var params = operator.innerText;
+
+  switch (params) {
+    case "+":
+      sum();
+      break;
+    case "-":
+      subtraction();
+      break;
+    case "*":
+      multiplication();
+      break;
+    case "/":
+      division();
+
+      break;
+
+    default:
+      alert(":@");
+      operator.innerHTML = ":x";
+
+      break;
+  }
+
+}
+
+function sum() {
+  const number_one = document.querySelector(".number_one");
+  const number_two = document.querySelector(".number_two");
+  const result = document.querySelector(".lab_result");
 
   var n1 = parseFloat(number_one.value);
   var n2 = parseFloat(number_two.value);
-  var r = n1 + n2;
 
-  alert(`A soma ta certa = ${r} só falta atualizar o DOM`);
-  
-  result.innerHTML = `${r}`;
+  result.innerHTML = n1 + n2;
+}
+
+function subtraction() {
+  const number_one = document.querySelector(".number_one");
+  const number_two = document.querySelector(".number_two");
+  const result = document.querySelector(".lab_result");
+
+  var n1 = parseFloat(number_one.value);
+  var n2 = parseFloat(number_two.value);
+
+  result.innerHTML = n1 - n2;
+}
+
+function division() {
+  const number_one = document.querySelector(".number_one");
+  const number_two = document.querySelector(".number_two");
+  const result = document.querySelector(".lab_result");
+
+  var n1 = parseFloat(number_one.value);
+  var n2 = parseFloat(number_two.value);
+
+  result.innerHTML = n1 / n2;
+}
+
+function multiplication() {
+  const number_one = document.querySelector(".number_one");
+  const number_two = document.querySelector(".number_two");
+  const result = document.querySelector(".lab_result");
+
+  var n1 = parseFloat(number_one.value);
+  var n2 = parseFloat(number_two.value);
+
+  result.innerHTML = n1 * n2;
 }
