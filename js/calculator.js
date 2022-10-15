674a7fee63939804f4941804424ae94313edc79e
Gabriel Machado <gramchado12@hotmail.com>
2022-10-14 21:04:57 -0400

    Update the calculator

---
+++ js/calculator.js
@@ -0,0 +1,12 @@
+function sum(params) {
+  var number_one = document.querySelector(".number_one");
+  var number_two = document.querySelector(".number_two");
+  var result = document.querySelector(".lab_result");
+
+  var n1 = parseFloat(number_one.value);
+  var n2 = parseFloat(number_two.value);
+  var r = n1 + n2;
+
+  alert(`${r}`);
+  result.innerHTML = `${r}`;
+}
