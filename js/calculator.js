5df66e0d8ea16cfeb0bfd70fa34655fe02833bd6
Gabriel Machado <grmachado12@hotmail.com>
2022-11-01 00:07:48 -0400

    feat: operators working

--- js/calculator.js
+++ js/calculator.js
@@ -1,6 +1,63 @@
+//keyboard calculator
+function keyboard(params) {
+  const input_one = document.querySelector(".number_one");
+  const input_tow = document.querySelector(".number_two");
+
+  switch (params) {
+    case "0":
+      input_one.value = 0;
+      break;
+    case "1":
+      input_one += "1";
+
+      break;
+    case "2":
+      input_one += "2";
+
+      break;
+    case "3":
+      break;
+    case "4":
+      break;
+    case "5":
+      break;
+    case "6":
+      break;
+    case "7":
+      break;
+    case "8":
+      break;
+    case "9":
+      break;
+
+    case "+":
+      operator("+");
+      break;
+    case "-":
+      operator("-");
+
+      break;
+    case "*":
+      operator("*");
+
+      break;
+    case "/":
+      operator("/");
+
+      break;
+    case ",":
+      break;
+    case "=":
+      result();
+      break;
+
+    default:
+      break;
+  }
+}
+
 //Change de operator.
 function operator(params) {
-
   const operator = document.querySelector(".lab_operator");
 
   switch (params) {
@@ -51,7 +108,6 @@ function result() {
 
       break;
   }
-
 }
 
 function sum() {
