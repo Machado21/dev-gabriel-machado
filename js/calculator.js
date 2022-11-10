bade8bbe2788208864d82b2d11d53f6b238ef366
Gabriel Machado <grmachado12@hotmail.com>
2022-11-10 00:21:40 -0400

    feat: keyboard working so so

--- js/calculator.js
+++ js/calculator.js
@@ -3,57 +3,63 @@ function keyboard(params) {
   const input_one = document.querySelector(".number_one");
   const input_tow = document.querySelector(".number_two");
 
-  switch (params) {
-    case "0":
-      input_one.value = 0;
-      break;
-    case "1":
-      input_one += "1";
+  
+    switch (params) {
+      case "0":
+        input_one.value += 0;
+        break;
+      case "1":
+        input_one.value += 1;
+        break;
+      case "2":
+        input_one.value += 2;
+        break;
+      case "3":
+        input_one.value += 3;
+        break;
+      case "4":
+        input_one.value += 4;
+        break;
+      case "5":
+        input_one.value += 5;
+        break;
+      case "6":
+        input_one.value += 6;
+        break;
+      case "7":
+        input_one.value += 7;
+        break;
+      case "8":
+        input_one.value += 8;
+        break;
+      case "9":
+        input_one.value += 9;
+        break;
 
-      break;
-    case "2":
-      input_one += "2";
+      case "+":
+        operator("+");
+        break;
+      case "-":
+        operator("-");
 
-      break;
-    case "3":
-      break;
-    case "4":
-      break;
-    case "5":
-      break;
-    case "6":
-      break;
-    case "7":
-      break;
-    case "8":
-      break;
-    case "9":
-      break;
+        break;
+      case "*":
+        operator("*");
 
-    case "+":
-      operator("+");
-      break;
-    case "-":
-      operator("-");
+        break;
+      case "/":
+        operator("/");
 
-      break;
-    case "*":
-      operator("*");
+        break;
+      case ",":
+        break;
+      case "=":
+        result();
+        break;
 
-      break;
-    case "/":
-      operator("/");
-
-      break;
-    case ",":
-      break;
-    case "=":
-      result();
-      break;
-
-    default:
-      break;
-  }
+      default:
+        break;
+    }
 }
 
 //Change de operator.
